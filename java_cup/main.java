a618673cbf2579da58585ef3c20fe159c53729b3
Kay-Khosrow <65471749+Kay-Khosrow@users.noreply.github.com>
2024-07-18 07:28:32 +0330

    Update main.java

--- java_cup/main.java
+++ java_cup/main.java
@@ -3,14 +3,14 @@
 // Licence: GPL-3.0
 //
 class Web {
-	public static void main (String[] args){
-		String appName = "AppName";
-		boolean isRunning = false;
+    public static void main (String[] args){
+    String appName = "AppName";
+    boolean isRunning = false;
 
-		System.out.println("Is running : " + isRunning + " " + appName+ "\n\n");
+    System.out.println("Is running : " + isRunning + " " + appName+ "\n\n");
     App nApp = new App();
     System.out.println("\n"+nApp);
-	}
+    }
 }
 
 class App {
